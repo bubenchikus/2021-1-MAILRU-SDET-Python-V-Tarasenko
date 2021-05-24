a4f8ece9ab7731d4029766eb454bd401a789e9b5
Valya <dozythoth@yandex.ru>
2021-05-24 16:48:57 +0300

    Исправления, попытка №2

--- Homework6/code/tests/test_mysql.py
+++ Homework6/code/tests/test_mysql.py
@@ -1,6 +1,8 @@
 import pytest
 
 from mysql.builder import MySQLBuilder
+from mysql.models import NumberOfAllRequests, NumberOfRequestsByType, Top10OfMostFrequentRequests, \
+    Top5OfBiggestRequestsWith4XXStatus, Top5OfUsersByQuantityWith5XXStatus
 
 
 class MySQLBase:
@@ -18,35 +20,35 @@ class MySQLBase:
 
 class TestMySQL(MySQLBase):
 
-    def template(self, name):
+    def template(self, model):
 
-        if not self.mysql.execute_query(f"SELECT * FROM {name}"):
+        if not self.mysql.session.query(model).first():
             self.mysql_builder.create_all_tables()
             self.mysql.connect()
 
-        return self.mysql.execute_query(f"SELECT * FROM {name}")
+        return self.mysql.session.query(model).all()
 
     def test_number_of_all_requests(self):
-        name = 'number_of_all_requests'
-        number_of_all_requests = self.template(name)
+        model = NumberOfAllRequests
+        number_of_all_requests = self.template(model)
         assert len(number_of_all_requests) == 1
 
     def test_number_of_requests_by_type(self):
-        name = 'number_of_requests_by_type'
-        number_of_requests_by_type = self.template(name)
+        model = NumberOfRequestsByType
+        number_of_requests_by_type = self.template(model)
         assert len(number_of_requests_by_type) == 5
 
     def test_top_10_of_most_frequent_requests(self):
-        name = 'top_10_of_most_frequent_requests'
-        top_10_of_most_frequent_requests = self.template(name)
+        model = Top10OfMostFrequentRequests
+        top_10_of_most_frequent_requests = self.template(model)
         assert len(top_10_of_most_frequent_requests) == 10
 
     def test_top_5_of_biggest_requests_with_4xx_status(self):
-        name = 'top_5_of_biggest_requests_with_4xx_status'
-        top_5_of_biggest_requests_with_4xx_status = self.template(name)
+        model = Top5OfBiggestRequestsWith4XXStatus
+        top_5_of_biggest_requests_with_4xx_status = self.template(model)
         assert len(top_5_of_biggest_requests_with_4xx_status) == 5
 
     def test_top_5_of_users_by_quantity_with_5xx_status(self):
-        name = 'top_5_of_users_by_quantity_with_5xx_status'
-        top_5_of_users_by_quantity_with_5xx_status = self.template(name)
+        model = Top5OfUsersByQuantityWith5XXStatus
+        top_5_of_users_by_quantity_with_5xx_status = self.template(model)
         assert len(top_5_of_users_by_quantity_with_5xx_status) == 5
